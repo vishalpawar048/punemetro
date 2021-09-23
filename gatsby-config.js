75d6501d05460c1e89cc3f4150a22447faa09c56
Vishal Pawar <vishalpawar@Vishals-MacBook-Air.local>
2021-09-23 14:35:46 +0530

    pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,20 +6,29 @@ module.exports = {
   plugins: [
     "gatsby-plugin-styled-components",
     "gatsby-plugin-image",
+    
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
         trackingId: "UA-147726857-1",
       },
     },
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        icon: "src/images/icon.png",
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.png`
       },
     },
+    "gatsby-plugin-offline",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
@@ -30,5 +39,6 @@ module.exports = {
       },
       __key: "images",
     },
+    
   ],
 };
